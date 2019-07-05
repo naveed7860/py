6a3cf6e36fb63ce32791dbf6c3c6bbbfab7fe330
shaik naveed ahamed <44160062+naveed7860@users.noreply.github.com>
2019-07-05 09:44:57 +0530

    Update and rename pyth.py to pyth1.py

--- pyth1.py
+++ pyth1.py
@@ -6,7 +6,8 @@ for i in range(len(q)-1):
   if q[i]==q[i+1]:
     m.append(q[i])
 if m:
-  for x in set(m):
-    print(x,end=" " )
-else:
+    for x in set(m):
+      print(x,end=" ")
+else: 
   print("unique")
+  
